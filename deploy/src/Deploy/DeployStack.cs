76cf31d00ef7fd2b3708fb9c2e79477f45d8a3b8
Hoskins, Brian <brian.hoskins@hidglobal.com>
2025-03-24 15:17:30 +0000

    Added CDK as a feature to the DevContainer and used it to create the skeleton C# CDK app

---
+++ deploy/src/Deploy/DeployStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace Deploy
+{
+    public class DeployStack : Stack
+    {
+        internal DeployStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
