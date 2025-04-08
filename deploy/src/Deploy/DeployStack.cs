c2244caa5e360a0ec08ad8e0512ee96bf469abb5
Hoskins, Brian <brian.hoskins@hidglobal.com>
2025-04-08 14:45:08 +0000

    Blazor app deploys successfully

--- deploy/src/Deploy/DeployStack.cs
+++ deploy/src/Deploy/DeployStack.cs
@@ -1,13 +1,46 @@
 using Amazon.CDK;
+using Amazon.CDK.AWS.Ecr.Assets;
+using Amazon.CDK.AWS.AppRunner.Alpha;
 using Constructs;
+using System.IO;
 
 namespace Deploy
 {
     public class DeployStack : Stack
     {
-        internal DeployStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        internal DeployStack(Construct scope, string id, IStackProps props = null)
+            : base(scope, id, props)
         {
-            // The code that defines your stack goes here
+            // Build and upload Docker image from Dockerfile.blazor
+            
+            var projectRoot = "/workspaces/BlazorDevContainerTest";
+
+            var dockerImage = new DockerImageAsset(this, "BlazorImage", new DockerImageAssetProps
+            {
+                Directory = projectRoot,
+                File = ".devcontainer/Dockerfile.blazor"
+            });
+
+
+            // App Runner service using that image
+            var service = new Service(this, "BlazorAppRunner", new ServiceProps
+            {
+                Source = Source.FromAsset(new AssetProps
+                {
+                    ImageConfiguration = new ImageConfiguration
+                    {
+                        Port = 8080
+                    },
+                    Asset = dockerImage
+                }),
+                AutoDeploymentsEnabled = true
+            });
+
+            new CfnOutput(this, "AppRunnerURL", new CfnOutputProps
+            {
+                Value = service.ServiceUrl,
+                Description = "Blazor App Public URL"
+            });
         }
     }
 }
--- deploy/src/Deploy/Program.cs
+++ deploy/src/Deploy/Program.cs
@@ -10,6 +10,9 @@ namespace Deploy
         public static void Main(string[] args)
         {
             var app = new App();
+
+
+
             new DeployStack(app, "DeployStack", new StackProps
             {
                 // If you don't specify 'env', this stack will be environment-agnostic.
@@ -18,13 +21,13 @@ namespace Deploy
 
                 // Uncomment the next block to specialize this stack for the AWS Account
                 // and Region that are implied by the current CLI configuration.
-                /*
+                
                 Env = new Amazon.CDK.Environment
                 {
                     Account = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_ACCOUNT"),
                     Region = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_REGION"),
                 }
-                */
+                
 
                 // Uncomment the next block if you know exactly what Account and Region you
                 // want to deploy the stack to.
